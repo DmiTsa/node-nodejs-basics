f0236a8e47149a69a6540aeea23fb3a0e3485b83
Dmitry Taskaev <89737095+DmiTsa@users.noreply.github.com>
2022-12-03 20:56:12 +0300

    WT section done

---
+++ src/wt/worker.js
@@ -0,0 +1,12 @@
+import { workerData, parentPort } from 'node:worker_threads';
+
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+const sendResult = () => {
+  parentPort.postMessage(nthFibonacci(workerData));
+};
+
+sendResult();
+
+// console.log('worker ', workerData);
